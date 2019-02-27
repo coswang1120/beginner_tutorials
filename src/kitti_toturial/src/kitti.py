1aa33fdf4934e361d0d9edda4303d5871928c28c
coswang <coswang1120@gmail.com>
2019-02-27 10:20:25 +0800

    update imu

--- src/kitti_toturial/src/kitti.py
+++ src/kitti_toturial/src/kitti.py
@@ -12,17 +12,24 @@ if __name__ == '__main__':
     rospy.init_node('kitti_node',anonymous=True) 
     cam_pub=rospy.Publisher('kitti_cam',Image,queue_size=10)
     pcl_pub=rospy.Publisher('kitti_point_cloud',PointCloud2,queue_size=10)
+    #ego_pub=rospy.Publisher('kitti_ego_car',Marker,queue_size=10)
+    ego_pub=rospy.Publisher('kitti_ego_car',MarkerArray,queue_size=10)
+    #model_pub=rospy.Publisher('kitti_car_model',Marker,queue_size=10)
+    imu_pub=rospy.Publisher('kitti_imu',Imu,queue_size=10)
     bridge=CvBridge()
 
     rate=rospy.Rate(10)
     while not rospy.is_shutdown():
-        img=cv2.imread(os.path.join(DATA_PATH,'image_02/data/%010d.png'%frame))
-        point_cloud=np.fromfile(os.path.join(DATA_PATH,'velodyne_points/data/%010d.bin'%frame),dtype=np.float32).reshape(-1,4)
-        cam_pub.publish(bridge.cv2_to_imgmsg(img,"bgr8"))
-        header=Header()
-        header.stamp=rospy.Time.now()
-        header.frame_id='map'
-        pcl_pub.publish(pcl2.create_cloud_xyz32(header,point_cloud[:,:3]))
+        image=read_camera(os.path.join(DATA_PATH,'image_02/data/%010d.png'%frame))
+        publish_camera(cam_pub,bridge,image)
+        
+        point_cloud=read_point_cloud(os.path.join(DATA_PATH,'velodyne_points/data/%010d.bin'%frame))
+        publish_point_cloud(pcl_pub,point_cloud)
+        publish_ego_car(ego_pub)
+        #publish_car_model(model_pub)
+
+        imu_data=read_imu(os.path.join(DATA_PATH,'oxts/data/%010d.txt'%frame))
+        publish_imu(imu_pub,imu_data)
         rospy.loginfo("published")
         rate.sleep()
         frame+=1
--- src/kitti_toturial/src/publish_utils.py
+++ src/kitti_toturial/src/publish_utils.py
@@ -6,12 +6,13 @@ import numpy as np
 
 import rospy
 from std_msgs.msg import Header
-from sensor_msgs.msg import Image,PointCloud2
+from sensor_msgs.msg import Image,PointCloud2,Imu
 from cv_bridge import CvBridge
 import sensor_msgs.point_cloud2 as pcl2
+from geometry_msgs.msg import Point
 
-
-from visualization_msgs.msg import Marker
+import tf
+from visualization_msgs.msg import Marker,MarkerArray
 
 FRAME_ID='map'
 
@@ -27,14 +28,17 @@ def publish_point_cloud(pcl_pub,point_cloud):
 
 
 def publish_ego_car(ego_car_pub):
-    maker=Maker()
-    maker.header.frame.id=FRAME_ID
-    maker.stamp=rospy.Time.now()
 
-    maker.id=0
-    maker.action=Marker.ADD
-    maker.lifttime = rospy.Duration()
-    marker.type=Maker.LINE_STRIP
+    marker_array=MarkerArray()
+
+    marker=Marker()
+    marker.header.frame_id=FRAME_ID
+    marker.header.stamp=rospy.Time.now()
+
+    marker.id=0
+    marker.action=Marker.ADD
+    marker.lifetime = rospy.Duration()
+    marker.type=Marker.LINE_STRIP
 
     marker.color.r=0.0
     marker.color.g=1.0
@@ -47,4 +51,89 @@ def publish_ego_car(ego_car_pub):
     marker.points.append(Point(0,0,0))
     marker.points.append(Point(10,10,0))
 
-    maker_array.makers.apped(marker)
+    marker_array.markers.append(marker)
+
+
+    #3D car
+    mesh_marker=Marker()
+    mesh_marker.header.frame_id=FRAME_ID
+    mesh_marker.header.stamp=rospy.Time.now()
+
+    mesh_marker.id=-1
+    mesh_marker.lifetime = rospy.Duration()
+    mesh_marker.type=Marker.MESH_RESOURCE
+    mesh_marker.mesh_resource="package://kitti_toturial/bmw_x5/BMW X5 4.dae"
+    
+    mesh_marker.pose.position.x=0.0
+    mesh_marker.pose.position.y=0.0
+    mesh_marker.pose.position.z=-1.73
+    
+    q=tf.transformations.quaternion_from_euler(np.pi/2,0,np.pi)
+    mesh_marker.pose.orientation.x=q[0]
+    mesh_marker.pose.orientation.y=q[1]
+    mesh_marker.pose.orientation.z=q[2]
+    mesh_marker.pose.orientation.w=q[3]
+   
+    mesh_marker.color.r=1.0
+    mesh_marker.color.g=1.0
+    mesh_marker.color.b=1.0
+    mesh_marker.color.a=1.0
+
+    mesh_marker.scale.x=0.9
+    mesh_marker.scale.y=0.9
+    mesh_marker.scale.z=0.9
+
+    marker_array.markers.append(mesh_marker)
+    ego_car_pub.publish(marker_array)
+
+def publish_car_model(model_pub):
+    mesh_marker=Marker()
+    mesh_marker.header.frame_id=FRAME_ID
+    mesh_marker.header.stamp=rospy.Time.now()
+
+    mesh_marker.id=-1
+    mesh_marker.lifetime = rospy.Duration()
+    mesh_marker.type=Marker.MESH_RESOURCE
+    mesh_marker.mesh_resource="package://kitti_toturial/bmw_x5/BMW X5 4.dae"
+    
+    mesh_marker.pose.position.x=0.0
+    mesh_marker.pose.position.y=0.0
+    mesh_marker.pose.position.z=-1.73
+    
+    q=tf.transformations.quaternion_from_euler(np.pi/2,0,np.pi)
+    mesh_marker.pose.orientation.x=q[0]
+    mesh_marker.pose.orientation.y=q[1]
+    mesh_marker.pose.orientation.z=q[2]
+    mesh_marker.pose.orientation.w=q[3]
+   
+    mesh_marker.color.r=1.0
+    mesh_marker.color.g=1.0
+    mesh_marker.color.b=1.0
+    mesh_marker.color.a=1.0
+
+    mesh_marker.scale.x=0.9
+    mesh_marker.scale.y=0.9
+    mesh_marker.scale.z=0.9
+   
+
+    model_pub.publish(mesh_marker) 
+
+
+def publish_imu(imu_pub,imu_data):       
+    imu=Imu()
+    imu.header.frame_id=FRAME_ID
+    imu.header.stamp=rospy.Time.now()
+    
+    q=tf.transformations.quaternion_from_euler(float(imu_data.roll),float(imu_data.pitch),float(imu_data.yaw))
+    imu.orientation.x=q[0]
+    imu.orientation.y=q[1]
+    imu.orientation.z=q[2]
+    imu.orientation.w=q[3]
+    imu.linear_acceleration.x=imu_data.af
+    imu.linear_acceleration.x=imu_data.al
+    imu.linear_acceleration.x=imu_data.au
+    imu.angular_velocity.x=imu_data.wf
+    imu.angular_velocity.y=imu_data.wl
+    imu.angular_velocity.z=imu_data.wu
+
+    imu_pub.publish(imu)

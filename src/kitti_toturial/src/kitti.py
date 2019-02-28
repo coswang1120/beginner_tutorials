f2964c4fb194ef3b0db715428dd533416ff58209
coswang <coswang1120@gmail.com>
2019-02-28 22:42:36 +0800

    update draw box

--- src/kitti_toturial/src/kitti.py
+++ src/kitti_toturial/src/kitti.py
@@ -4,8 +4,30 @@
 
 from data_utils import *
 from publish_utils import *
+from kitti_utils import *
 
 DATA_PATH='/mnt/data/prog/Kitti/2011_09_26 /2011_09_26_drive_0005_sync/'
+
+class Object():
+    def __init__(self):
+        seld.locations=[]
+        
+    def update(self,displacement,yaw):
+        for i in range(len(locations)):
+            x0,y0=locations[i]
+            x1=x0*np.cos(yaw_change)+y0*np.sin(yaw_change)-displacement
+            y1=-x0*np.sin(yaw_change)+y0*np.cos(yaw_change)
+            locations[i]=np.array([x1,y1])     
+
+def compute_3d_box_cam2(h,w,l,x,y,z,yaw):
+    R=np.array([[np.cos(yaw),0,np.sin(yaw)],[0,1,0],[-np.sin(yaw),0,np.cos(yaw)]])
+    x_corners = [l/2, l/2, -l/2, -l/2, l/2, l/2, -l/2, -l/2]
+    y_corners = [0, 0, 0, 0, -h, -h, -h, -h ]
+    z_corners = [w/2, -w/2, -w/2, w/2, w/2, -w/2, -w/2, w/2 ]
+    corners_3d_cam2=np.dot(R,np.vstack([x_corners,y_corners,z_corners]))
+    corners_3d_cam2+=np.vstack([x,y,z])
+    return corners_3d_cam2
+    
 
 if __name__ == '__main__':
     frame = 0
@@ -16,20 +38,43 @@ if __name__ == '__main__':
     ego_pub=rospy.Publisher('kitti_ego_car',MarkerArray,queue_size=10)
     #model_pub=rospy.Publisher('kitti_car_model',Marker,queue_size=10)
     imu_pub=rospy.Publisher('kitti_imu',Imu,queue_size=10)
+    gps_pub=rospy.Publisher('kitti_gps',NavSatFix,queue_size=10)
+    box3d_pub=rospy.Publisher('kitti_3d',MarkerArray,queue_size=10)
+    
+    
+    
     bridge=CvBridge()
 
+
+    df_tracking=read_tracking('/mnt/data/prog/Kitti/label_02/0000.txt')
+    calib=Calibration('/mnt/data/prog/Kitti/2011_09_26 /2011_09_26_drive_0005_sync/',from_video=True)
+    
     rate=rospy.Rate(10)
     while not rospy.is_shutdown():
+        df_tracking_frame=df_tracking[df_tracking.frame==frame]
+        
+        boxes_2d=np.array(df_tracking[df_tracking.frame==frame][['bbox_left','bbox_top','bbox_right','bbox_bottom']])
+
+        types=np.array(df_tracking[df_tracking.frame==frame][['type']])
+        boxes_3d=np.array(df_tracking_frame[['height','width','length','pos_x','pos_y','pos_z','rot_y']])
+        track_ids=np.array(df_tracking_frame['track_id'])
+        corners_3d_velos=[]
+        for box_3d in boxes_3d:
+            corners_3d_cam2= compute_3d_box_cam2(*box_3d)
+            corners_3d_velo=calib.project_rect_to_velo(corners_3d_cam2.T)
+            corners_3d_velos+=[corners_3d_velo]
+        
         image=read_camera(os.path.join(DATA_PATH,'image_02/data/%010d.png'%frame))
-        publish_camera(cam_pub,bridge,image)
+        publish_camera(cam_pub,bridge,image,boxes_2d,types)
         
         point_cloud=read_point_cloud(os.path.join(DATA_PATH,'velodyne_points/data/%010d.bin'%frame))
         publish_point_cloud(pcl_pub,point_cloud)
         publish_ego_car(ego_pub)
         #publish_car_model(model_pub)
-
+        publish_3dbox(box3d_pub,corners_3d_velos,types,track_ids)
         imu_data=read_imu(os.path.join(DATA_PATH,'oxts/data/%010d.txt'%frame))
         publish_imu(imu_pub,imu_data)
+        publish_gps(gps_pub,imu_data)
         rospy.loginfo("published")
         rate.sleep()
         frame+=1
--- src/kitti_toturial/src/publish_utils.py
+++ src/kitti_toturial/src/publish_utils.py
@@ -6,7 +6,7 @@ import numpy as np
 
 import rospy
 from std_msgs.msg import Header
-from sensor_msgs.msg import Image,PointCloud2,Imu
+from sensor_msgs.msg import Image,PointCloud2,Imu,NavSatFix
 from cv_bridge import CvBridge
 import sensor_msgs.point_cloud2 as pcl2
 from geometry_msgs.msg import Point
@@ -15,8 +15,23 @@ import tf
 from visualization_msgs.msg import Marker,MarkerArray
 
 FRAME_ID='map'
+LIFETIME=0.1
 
-def publish_camera(cam_pub,bridge,image):
+LINES = [[0,1],[1,2],[2,3],[3,0]]  #draw box left below corner is zero  clockwise below to top
+LINES +=[[4,5],[5,6],[6,7],[7,4]]
+LINES +=[[0,4],[1,5],[2,6],[3,7]] 
+LINES +=[[4,1],[5,0]]
+
+def publish_camera(cam_pub,bridge,image,boxes,types):
+    for typ,box in zip(types,boxes):
+        top_left=int(box[0]),int(box[1])
+        bottom_right=int(box[2]),int(box[3])
+        if np.array_equal(typ,['Car']):
+            cv2.rectangle(image,top_left,bottom_right,(255,255,0),2)
+        elif np.array_equal(typ,['Pedestrian']):
+            cv2.rectangle(image,top_left,bottom_right,(0,226,0),2)
+        else:
+            cv2.rectangle(image,top_left,bottom_right,(141,255,0),2)
     cam_pub.publish(bridge.cv2_to_imgmsg(image,"bgr8"))
 
 
@@ -137,3 +152,104 @@ def publish_imu(imu_pub,imu_data):
     imu.angular_velocity.z=imu_data.wu
 
     imu_pub.publish(imu)
+
+def publish_gps(gps_pub,imu_data):       
+    gps=NavSatFix()
+    gps.header.frame_id=FRAME_ID
+    gps.header.stamp=rospy.Time.now()
+    
+    gps.latitude=imu_data.lat
+    gps.longitude=imu_data.lon
+    gps.altitude=imu_data.alt
+
+    gps_pub.publish(gps)  
+def publish_3dbox(box3d_pub,corners_3d_velos,types,track_ids):
+    marker_array=MarkerArray()  
+    for i,corners_3d_velo in enumerate(corners_3d_velos):
+        marker=Marker()
+        marker.header.frame_id=FRAME_ID
+        marker.header.stamp=rospy.Time.now()
+
+        marker.id=i
+        marker.action=Marker.ADD
+        marker.lifetime=rospy.Duration(LIFETIME)
+        marker.type=Marker.LINE_LIST
+
+        if np.array_equal(types[i],['Car']):
+            b=255
+            g=255
+            r=0   
+        elif np.array_equal(types[i],['Pedestrian']):
+            b=0
+            g=226
+            r=0           
+        else:
+            b=141
+            g=255
+            r=0 
+            
+        marker.color.r=r/255.0
+        marker.color.g=g/255.0
+        marker.color.b=b/255.0
+
+        marker.color.a=1.0
+
+        marker.scale.x=0.1
+
+        marker.points=[]
+       
+        for l in LINES:
+            
+            p1=corners_3d_velo[l[0]]            
+            marker.points.append(Point(p1[0],p1[1],p1[2]))
+            p2=corners_3d_velo[l[1]]
+            marker.points.append(Point(p2[0],p2[1],p2[2]))
+        marker_array.markers.append(marker)
+          
+    
+    text_marker=Marker()
+    text_marker.header.frame_id=FRAME_ID
+    text_marker.header.stamp=rospy.Time.now()
+
+    text_marker.id=i+1000
+    text_marker.action=Marker.ADD
+    text_marker.lifetime=rospy.Duration(LIFETIME)
+    text_marker.type=Marker.TEXT_VIEW_FACING
+
+    p=np.mean(corners_3d_velo,axis=0)  
+    text_marker.pose.position.x = p[0]
+    text_marker.pose.position.y = p[1]
+    text_marker.pose.position.z = p[2]+1
+
+    text_marker.text=str(track_ids[i])
+
+    text_marker.scale.x=1
+    text_marker.scale.y=1
+    text_marker.scale.z=1
+
+    
+    text_marker.scale.x=1
+    text_marker.scale.y=1
+    text_marker.scale.z=1
+    
+    if np.array_equal(types[i],['Car']):
+        b=255
+        g=255
+        r=0   
+    elif np.array_equal(types[i],['Pedestrian']):
+        b=0
+        g=226
+        r=0           
+    else:
+        b=141
+        g=255
+        r=0 
+            
+    text_marker.color.r=r/255.0
+    text_marker.color.g=g/255.0
+    text_marker.color.b=b/255.0
+    text_marker.color.a=1.0
+
+    marker_array.markers.append(text_marker)
+    box3d_pub.publish(marker_array) 
+        

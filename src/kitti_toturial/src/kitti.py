d26f1576bf67a4c8b55874d8ee46906e719bc881
coswang1120 <coswang1120@gmail.com>
2024-10-10 13:07:53 +0800

    Update execution counts in track_id.ipynb
    Modified execution counts across multiple code cells to reflect current execution sequence. Added a new raw cell and inserted a new code cell with its corresponding output.

--- src/kitti_toturial/src/kitti.py
+++ src/kitti_toturial/src/kitti.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python
 
 
-
+from collections import deque
 from data_utils import *
 from publish_utils import *
 from kitti_utils import *
+from misc import *
+
+EGOCAR=np.array([[2.15,0.9,-1.73],[2.15,-0.9,-1.73],[-1.95,-0.9,-1.73],[-1.95,0.9,-1.73],
+                 [2.15,0.9,-0.23],[2.15,-0.9,-0.23],[-1.95,-0.9,-0.23],[-1.95,0.9,-0.23]])
 
 DATA_PATH='/mnt/data/prog/Kitti/2011_09_26 /2011_09_26_drive_0005_sync/'
 
 class Object():
-    def __init__(self):
-        seld.locations=[]
-        
-    def update(self,displacement,yaw):
-        for i in range(len(locations)):
-            x0,y0=locations[i]
+    def __init__(self,center):
+        self.locations=deque(maxlen=20)
+        self.locations.appendleft(center)
+
+    def update(self,center,displacement,yaw):
+        for i in range(len(self.locations)):
+            x0,y0=self.locations[i]
             x1=x0*np.cos(yaw_change)+y0*np.sin(yaw_change)-displacement
             y1=-x0*np.sin(yaw_change)+y0*np.cos(yaw_change)
-            locations[i]=np.array([x1,y1])     
+            self.locations[i]=np.array([x1,y1])  
+       
+        if center is not None:
+            self.locations.appendleft(center)  
+    def reset(self):
+        self.locations=deque(maxlen=20)
 
-def compute_3d_box_cam2(h,w,l,x,y,z,yaw):
-    R=np.array([[np.cos(yaw),0,np.sin(yaw)],[0,1,0],[-np.sin(yaw),0,np.cos(yaw)]])
-    x_corners = [l/2, l/2, -l/2, -l/2, l/2, l/2, -l/2, -l/2]
-    y_corners = [0, 0, 0, 0, -h, -h, -h, -h ]
-    z_corners = [w/2, -w/2, -w/2, w/2, w/2, -w/2, -w/2, w/2 ]
-    corners_3d_cam2=np.dot(R,np.vstack([x_corners,y_corners,z_corners]))
-    corners_3d_cam2+=np.vstack([x,y,z])
-    return corners_3d_cam2
+
     
 
 if __name__ == '__main__':
@@ -40,43 +43,80 @@ if __name__ == '__main__':
     imu_pub=rospy.Publisher('kitti_imu',Imu,queue_size=10)
     gps_pub=rospy.Publisher('kitti_gps',NavSatFix,queue_size=10)
     box3d_pub=rospy.Publisher('kitti_3d',MarkerArray,queue_size=10)
-    
-    
-    
+    loc_pub=rospy.Publisher('kitti_loc',MarkerArray,queue_size=10)
+    dist_pub=rospy.Publisher('kitti_dist',MarkerArray,queue_size=10)
     bridge=CvBridge()
 
+    rate=rospy.Rate(10)
 
     df_tracking=read_tracking('/mnt/data/prog/Kitti/label_02/0000.txt')
     calib=Calibration('/mnt/data/prog/Kitti/2011_09_26 /2011_09_26_drive_0005_sync/',from_video=True)
     
-    rate=rospy.Rate(10)
+    #ego_car=Object()
+    tracker = {}
+    pre_imu_data=None
+
     while not rospy.is_shutdown():
         df_tracking_frame=df_tracking[df_tracking.frame==frame]
         
         boxes_2d=np.array(df_tracking[df_tracking.frame==frame][['bbox_left','bbox_top','bbox_right','bbox_bottom']])
-
         types=np.array(df_tracking[df_tracking.frame==frame][['type']])
         boxes_3d=np.array(df_tracking_frame[['height','width','length','pos_x','pos_y','pos_z','rot_y']])
         track_ids=np.array(df_tracking_frame['track_id'])
+        
         corners_3d_velos=[]
-        for box_3d in boxes_3d:
+        centers={}
+        minPQDs=[]
+        for track_id,box_3d in zip(track_ids,boxes_3d):
             corners_3d_cam2= compute_3d_box_cam2(*box_3d)
             corners_3d_velo=calib.project_rect_to_velo(corners_3d_cam2.T)
-            corners_3d_velos+=[corners_3d_velo]
+            minPQDs += [min_distnace_cubolds(EGOCAR,corners_3d_velo)]
+            corners_3d_velos += [corners_3d_velo]
+            centers[track_id]=np.mean(corners_3d_velo,axis=0)[:2]
         
+        corners_3d_velos+=[EGOCAR]
+        types=np.append(types,'Car')
+        track_ids=np.append(track_ids,-1)
+        centers[-1]=np.array([0,0])    
+
         image=read_camera(os.path.join(DATA_PATH,'image_02/data/%010d.png'%frame))
-        publish_camera(cam_pub,bridge,image,boxes_2d,types)
-        
         point_cloud=read_point_cloud(os.path.join(DATA_PATH,'velodyne_points/data/%010d.bin'%frame))
-        publish_point_cloud(pcl_pub,point_cloud)
-        publish_ego_car(ego_pub)
-        #publish_car_model(model_pub)
-        publish_3dbox(box3d_pub,corners_3d_velos,types,track_ids)
         imu_data=read_imu(os.path.join(DATA_PATH,'oxts/data/%010d.txt'%frame))
+        
+        if pre_imu_data is None:
+            for track_id in centers:
+                tracker[track_id] = Object(centers[track_id])
+        else:
+            displacement=0.1*np.linalg.norm(imu_data[['vf','vl']])
+            yaw_change=float(imu_data.yaw-pre_imu_data.yaw)
+            for track_id in centers:
+                if track_id in tracker:
+                    tracker[track_id].update(centers[track_id],displacement,yaw_change)           
+                else:
+                    tracker[track_id] = Object(centers[track_id])
+            for track_id in tracker:
+                if track_id not in centers:
+                    tracker[track_id].update(None,displacement,yaw_change)                    
+
+
+
+
+        pre_imu_data=imu_data
+   
+        publish_camera(cam_pub,bridge,image,boxes_2d,types)
+        publish_point_cloud(pcl_pub,point_cloud[::2]) 
+        publish_3dbox(box3d_pub,corners_3d_velos,types,track_ids)
+        publish_ego_car(ego_pub)
         publish_imu(imu_pub,imu_data)
         publish_gps(gps_pub,imu_data)
+        publish_loc(loc_pub,tracker,centers)
+        publish_dist(dist_pub,minPQDs)
+
         rospy.loginfo("published")
         rate.sleep()
         frame+=1
-        frame%=154
+        if frame==154:
+            frame=0
+            for track_id in tracker:
+                tracker[track_id].reset()
 
--- src/kitti_toturial/src/publish_utils.py
+++ src/kitti_toturial/src/publish_utils.py
@@ -228,9 +228,9 @@ def publish_3dbox(box3d_pub,corners_3d_velos,types,track_ids):
     text_marker.scale.z=1
 
     
-    text_marker.scale.x=1
-    text_marker.scale.y=1
-    text_marker.scale.z=1
+    #text_marker.scale.x=1
+    #text_marker.scale.y=1
+    #text_marker.scale.z=1
     
     if np.array_equal(types[i],['Car']):
         b=255
@@ -252,4 +252,93 @@ def publish_3dbox(box3d_pub,corners_3d_velos,types,track_ids):
 
     marker_array.markers.append(text_marker)
     box3d_pub.publish(marker_array) 
+
+
+def publish_loc(loc_pub,tracker,centers):
+
+    marker_array=MarkerArray()
+    
+    for track_id in centers:
+        marker=Marker()
+        marker.header.frame_id=FRAME_ID
+        marker.header.stamp=rospy.Time.now()
+
+        marker.action=Marker.ADD
+        marker.lifetime = rospy.Duration(LIFETIME)
+        marker.type=Marker.LINE_STRIP
+        marker.id=track_id
+
+        marker.color.r=1.0
+        marker.color.g=1.0
+        marker.color.b=0.0
+        marker.color.a=0.8
+        marker.scale.x=0.1
+
+        marker.points=[]
+        for p in tracker[track_id].locations:
+            marker.points.append(Point(p[0],p[1],0))
+
         
+        marker_array.markers.append(marker)
+    loc_pub.publish(marker_array)    
+
+
+
+
+def publish_dist(dist_pub,minPQDs):
+
+    marker_array=MarkerArray()
+    
+    for i, (minP,minQ, minD) in enumerate(minPQDs):
+        marker=Marker()
+        marker.header.frame_id=FRAME_ID
+        marker.header.stamp=rospy.Time.now()
+
+        marker.action=Marker.ADD
+        marker.lifetime = rospy.Duration(LIFETIME)
+        marker.type=Marker.LINE_STRIP
+        marker.id=i
+
+        marker.color.r=1.0
+        marker.color.g=0.0
+        marker.color.b=1.0
+        marker.color.a=0.5
+        marker.scale.x=0.1
+
+        marker.points=[]
+        marker.points.append(Point(minP[0],minP[1],0))        
+        marker.points.append(Point(minQ[0],minQ[1],0))
+        
+        marker_array.markers.append(marker)
+
+
+        text_marker=Marker()
+        text_marker.header.frame_id=FRAME_ID
+        text_marker.header.stamp=rospy.Time.now()
+
+        text_marker.id=i+1000
+        text_marker.action=Marker.ADD
+        text_marker.lifetime=rospy.Duration(LIFETIME)
+        text_marker.type=Marker.TEXT_VIEW_FACING
+
+        p=(minP+minQ)/2
+        text_marker.pose.position.x = p[0]
+        text_marker.pose.position.y = p[1]
+        text_marker.pose.position.z = 0
+
+        text_marker.text='%.2f'%minD
+
+        text_marker.scale.x=1
+        text_marker.scale.y=1
+        text_marker.scale.z=1
+
+               
+        text_marker.color.r=1.0
+        text_marker.color.g=1.0
+        text_marker.color.b=1.0
+        text_marker.color.a=0.8
+
+        marker_array.markers.append(text_marker)
+
+
+    dist_pub.publish(marker_array)   
